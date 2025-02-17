eaef032456cbcab3d32398ca99ae36d76cb7a709
marcosdidier <160912875+marcosdidier@users.noreply.github.com>
2025-02-17 00:21:49 -0300

    Created Tarefas's methods

--- models/tarefas.py
+++ models/tarefas.py
@@ -1,10 +1,58 @@
-class Task:
-    _id_counter_ = 1
+import sqlite3
+from models.usuario import Usuario
 
-    STATUS = ["Pendente", "Em andamento", "Concluído"]
+class Tarefas:
+    OPCOES_STATUS = ["Pendente", "Em andamento", "Concluída"]
 
-    def __init__(self, title:str, desc:str, user):
-        self.id = Task._id_counter_
-        self.title = title
-        self.desc = desc
+    @staticmethod
+    def criar(titulo, descricao, usuario_id):
+        conn = sqlite3.connect("task_manager.db")
+        cursor = conn.cursor()
+
+        usuario = Usuario.buscar_por_id(usuario_id)
+
+        if not usuario:
+            print("Usuário não encontrado!")
+            return
+        
+        cursor.execute("INSERT INTO tarefas (titulo, descricao, usuario_id) VALUES (?, ?, ?)", (titulo, descricao, usuario_id))
+        conn.commit()
+        conn.close()
+        print("Tarefa cadastrada com sucesso!")
+
+    @staticmethod
+    def listar():
+        conn = sqlite3.connect("task_manager.db")
+        cursor = conn.cursor()
+
+        cursor.execute("""
+        SELECT tarefas.id, tarefas.titulo, tarefas.status, usuarios.nome
+        FROM tarefas
+        JOIN usuarios ON tarefas.usuario_id = usuarios_id
+        """)
+
+        tarefas = cursor.fetchall()
+        conn.close()
+        return tarefas
+    
+    @staticmethod
+    def atualizar_status(id_tarefa, novo_status):
+        if novo_status not in Tarefas.OPCOES_STATUS:
+            print("Status inválido!")
+            return
+        
+        conn = sqlite3.connect("task_manager.db")
+        cursor = conn.cursor()
+
+        cursor.execute("UPDATE tarefas SET status = ? WHERE id = ?", (novo_status, id_tarefa))
+        conn.commit()
+        conn.close()
+        print(f"Status da tarefa {id_tarefa} atualizado para {novo_status}")
+
+        
+        
+
+    
+
+
         
