3da70b3b9c95b6f176dceced7c6b6ddcdcf88b2f
marcosdidier <160912875+marcosdidier@users.noreply.github.com>
2025-02-16 23:15:13 -0300

    Creating the database

---
+++ models/database.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+conn = sqlite3.connect("task_manager.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS usuarios (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT NOT NULL,
+        email TEXT UNIQUE NOT NULL
+    )
+""")
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS tarefas (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        titulo TEXT NOT NULL,
+        descricao TEXT NOT NULL,
+        status TEXT NOT NULL DEFAULT 'Pendente',
+        usuario_id INTEGER,
+        FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+    )
+""")
+
+conn.commit()
+conn.close()
